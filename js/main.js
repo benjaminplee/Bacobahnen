b4783095974be76685641b794d030ab8b507d38f
Benjamin Lee <leebenjp@gmail.com>
2011-03-11 09:12:50 -0600

    Added replace event binding plugin to jQuery

--- js/main.js
+++ js/main.js
@@ -1,40 +1,51 @@
-
-function addBlock() {
-	var text = prompt("What would you like the block to say?")
+(function($) {
 	
-	if(text) {
-		$(this).toggleClass('block').html(text).unbind('click').bind('click', removeBlock)
+	$.fn.replace = function(eventType, newHandler) {
+		this.unbind(eventType).bind(eventType, newHandler)
 	}
 	
-	return false;
-}
+	function addBlock() {
+		var text = prompt("What would you like the block to say?")
 
-function removeBlock() {
-	$(this).toggleClass('block').html('').unbind('click').bind('click', addBlock)
-	
-	return false;
-}
-
-$('.grid td').live('click', addBlock)
-
-function gridBuilder(tableSelector, cols, rows) {
-	return function() {
-		var table = $(tableSelector)
-	
-		var gridText = ''
-	
-		for(var row = 0; row < rows; row++) {
-			gridText += '<tr>'
-		
-			for(var col = 0; col < cols; col++) {
-				gridText += '<td></td>'
-			}
-		
-			gridText += '</tr>'
+		if(text) {
+			$(this).toggleClass('block').html(text).replace('click', removeBlock)
 		}
-	
-		table.append('<tbody>' + gridText + '</tbody')
-	}
-}
 
-$(gridBuilder('#grid', 5, 5))
+		return false;
+	}
+
+	function removeBlock() {
+		$(this).toggleClass('block').html('').replace('click', addBlock)
+
+		return false;
+	}
+
+	function replace() {
+
+	}
+
+	$('.grid td').live('click', addBlock)
+
+	function gridBuilder(tableSelector, cols, rows) {
+		return function() {
+			var table = $(tableSelector)
+
+			var gridText = ''
+
+			for(var row = 0; row < rows; row++) {
+				gridText += '<tr>'
+
+				for(var col = 0; col < cols; col++) {
+					gridText += '<td></td>'
+				}
+
+				gridText += '</tr>'
+			}
+
+			table.append('<tbody>' + gridText + '</tbody')
+		}
+	}
+
+	$(gridBuilder('#grid', 5, 5))
+
+})(jQuery)
