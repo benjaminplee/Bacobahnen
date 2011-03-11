507bce2664f6494508611ab72c214fda8dd4317d
Benjamin Lee <leebenjp@gmail.com>
2011-03-10 22:00:09 -0600

    Removed internal grid representation and row/col passing in favor or single object. Replaced IF w/ bind/unbind

--- js/main.js
+++ js/main.js
@@ -1,52 +1,21 @@
-function toggleBlock() {
-	// parentNode is in level 1 DOM support
-	// rowIndex and cellIndex are supported DOM HTML properties
 
-	var col = this.cellIndex
-	var row = this.parentNode.rowIndex 
-
-	// alert('TD @ (' + col + ',' + row + ')')
-	
-	if(!blockIsAt(col, row)) {
-		addBlockAt(col, row)
-	}
-	else {
-		removeBlockAt(col, row)
-	}
-}
-
-var grid = {}
-
-function blockIsAt(col, row) {
-	return grid[col.toString()] && grid[col][row]
-}
-
-function addBlockAt(col, row) {
+function addBlock() {
 	var text = prompt("What would you like the block to say?")
 	
 	if(text) {
-		toggleBlockUI(col, row)
-		insertTextIntoBlock(text, col, row)
-		grid[col] = grid[col] || {}
-		grid[col][row] = true;
+		$(this).toggleClass('block').html(text).unbind('click').bind('click', removeBlock)
 	}
+	
+	return false;
 }
 
-function removeBlockAt(col, row) {
-	if(confirm("Delete this block?")) {
-		grid[col][row] = undefined
-		toggleBlockUI(col, row)
-		insertTextIntoBlock('', col, row)
-	}
+function removeBlock() {
+	$(this).toggleClass('block').html('').unbind('click').bind('click', addBlock)
+	
+	return false;
 }
 
-function toggleBlockUI(col, row) {
-	$('.grid tr:eq(' + row + ') td:eq(' + col + ')').toggleClass('block')
-}
-
-function insertTextIntoBlock(text, col, row) {
-	$('.grid tr:eq(' + row + ') td:eq(' + col + ')').html(text)
-}
+$('.grid td').live('click', addBlock)
 
 function gridBuilder(tableSelector, cols, rows) {
 	return function() {
@@ -69,4 +38,3 @@ function gridBuilder(tableSelector, cols, rows) {
 }
 
 $(gridBuilder('#grid', 5, 5))
-$('.grid td').live('click', toggleBlock)
