7365d4ae6bf3e212a3b96dc024c7ef23574045f6
Benjamin Lee <leebenjp@gmail.com>
2011-03-17 17:08:23 -0500

    cleaned up contentEditable work

--- js/main.js
+++ js/main.js
@@ -1,36 +1,9 @@
 (function($) {
 	
-	$('.editable').live('focus', function() {
-		$(this).addClass('block')
-		this.designMode = 'on';
-	}).live('blur', function() {
-		if(!$(this).html().trim()) {
-			$(this).removeClass('block')
+	$('.grid td').live('keyup', function() {
+		if(!$(this).text().trim()) {
+			$(this).html('')
 		}
-		
-		this.designMode = 'off';
 	})
-
-	function gridBuilder(tableSelector, cols, rows) {
-		return function() {
-			var table = $(tableSelector)
-
-			var gridText = ''
-
-			for(var row = 0; row < rows; row++) {
-				gridText += '<tr>'
-
-				for(var col = 0; col < cols; col++) {
-					gridText += '<td contenteditable="true" class="editable"></td>'
-				}
-
-				gridText += '</tr>'
-			}
-
-			table.append('<tbody>' + gridText + '</tbody')
-		}
-	}
-
-	$(gridBuilder('#grid', 5, 5))
 
 })(jQuery)
