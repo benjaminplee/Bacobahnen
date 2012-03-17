ab953891ce2119f699fffb74bb4db10734dddf2a
Benjamin P Lee <yardspoon@gmail.com>
2012-03-17 14:52:54 -0500

    multiple cards, sample layout, sample colors

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,13 @@
 $(function() {
+  var grid_size = 170; // article width + 2 * margin (need to make this not known to js)
+
 	$(".draggable").draggable({
-    grid: [170, 170],
+    grid: [grid_size, grid_size],
     stack: ".draggable"
+  });
+
+  // just for examples
+  $("article").each(function(i) {
+    $(this).css({"left": i * grid_size});
   });
 })
