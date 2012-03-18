4c5cac007857fc308d57319bda44960c6e49c04e
Benjamin P Lee <yardspoon@gmail.com>
2012-03-18 17:07:18 -0500

    changed height, need to create single authority on card properties

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,13 @@
 $(function() {
-  var grid_size = 170; // article width + 2 * margin (need to make this not known to js)
+  var grid_size = {x: 170, y: 95}; // article width + 2 * margin (need to make this not known to js)
 
 	$(".draggable").draggable({
-    grid: [grid_size, grid_size],
+    grid: [grid_size.x, grid_size.y],
     stack: ".draggable"
   });
 
   // just for examples
   $("article").each(function(i) {
-    $(this).css({"left": i * grid_size});
+    $(this).css({"left": i * grid_size.x});
   });
 })
